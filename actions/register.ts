2b42d64b789ab5e9bd1645ee5e8ee0868f088dbf
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-03-20 08:32:57 +0100

    Update actions

--- actions/register.ts
+++ actions/register.ts
@@ -1,5 +1,11 @@
+"use server";
+
+import bcrypt from "bcrypt";
 import { z } from "zod";
+
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -7,6 +13,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     if (!validatedFields.success) {
         return { error: "Invalid fields!" };
     }
+
+    const { email, password, name } = validatedFields.data;
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error: "Email already in use" };
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await db.user.create({
+       data: {
+           name,
+           email,
+           password: hashedPassword,
+       },
+    });
 
     return { success: "Confirmation email sent!" };
 }
