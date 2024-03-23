b6f695e800a4ccd9bbac189e08645dec82c17be4
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-03-23 18:10:43 +0100

    Finishing callbacks

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import { NextAuthConfig } from "next-auth";
 import Github from "next-auth/providers/github";
-import Credential from "next-auth/providers/credentials";
+import Credentials from "next-auth/providers/credentials";
 
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
@@ -12,31 +12,26 @@ export default {
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
         }),
-        Credential({
+        Credentials({
             async authorize(credentials) {
-                const validateFields = LoginSchema.safeParse(credentials);
+                const validatedFields = LoginSchema.safeParse(credentials);
 
-                if (validateFields.success) {
-                    const { email, password } = validateFields.data;
+                if (validatedFields.success) {
+                    const { email, password } = validatedFields.data;
 
                     const user = await getUserByEmail(email);
-
-                    if (!user || !user.password) {
-                        return null;
-                    }
+                    if (!user || !user.password) return null;
 
                     const passwordsMatch = await bcrypt.compare(
                         password,
                         user.password,
                     );
 
-                    if (passwordsMatch) {
-                        return null;
-                    }
-
-                    return null;
+                    if (passwordsMatch) return user;
                 }
+
+                return null;
             }
-        }),
+        })
     ],
 } satisfies NextAuthConfig;
